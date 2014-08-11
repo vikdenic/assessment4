78f6156c0358b3233ef2f0013b22e76a54ddf1b8
vikdenic <vik.denic@gmail.com>
2014-08-11 16:27:01 -0500

    Assessment4 project complete

--- Assessment4/ViewController.m
+++ Assessment4/ViewController.m
@@ -12,6 +12,9 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *myTableView;
 
+@property UIAlertView *addAlert;
+@property UIAlertView *colorAlert;
+
 @end
 
 @implementation ViewController
@@ -42,24 +45,58 @@
 //METHOD FOR PRESENTING ALERT VIEW WITH TEXT FIELD FOR USER TO ENTER NEW PERSON
 - (IBAction)onAddButtonTapped:(UIBarButtonItem *)sender
 {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Add a Person"
+    self.addAlert = [[UIAlertView alloc] initWithTitle:@"Add a Person"
                                                     message:nil
                                                    delegate:self
                                           cancelButtonTitle:@"Cancel"
                                           otherButtonTitles:@"Add", nil];
-    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
-    UITextField *alertTextField = [alert textFieldAtIndex:0];
+    self.addAlert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    UITextField *alertTextField = [self.addAlert textFieldAtIndex:0];
     alertTextField.keyboardType = UIKeyboardTypeDefault;
 
-    [alert show];
+    self.addAlert.tag = 0;
+    [self.addAlert show];
 }
 
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    if (buttonIndex != alertView.cancelButtonIndex)
+    if (buttonIndex != alertView.cancelButtonIndex && alertView.tag == 0)
     {
         //TODO: ADD YOUR CODE HERE FOR WHEN USER ADDS NEW PERSON
     }
+
+    //TODO: SAVE USER'S DEFAULT COLOR PREFERENCE USING THE CONDITIONAL BELOW
+    else if (alertView.tag == 1)
+    {
+        if (buttonIndex == 0)
+        {
+            self.navigationController.navigationBar.tintColor = [UIColor purpleColor];
+        }
+        else if (buttonIndex == 1)
+        {
+            self.navigationController.navigationBar.tintColor = [UIColor blueColor];
+        }
+        else if (buttonIndex == 2)
+        {
+            self.navigationController.navigationBar.tintColor = [UIColor orangeColor];
+        }
+        else if (buttonIndex == 3)
+        {
+            self.navigationController.navigationBar.tintColor = [UIColor greenColor];
+        }
+    }
+}
+
+//METHOD FOR PRESENTING USER'S COLOR PREFERENCE
+- (IBAction)onColorButtonTapped:(UIBarButtonItem *)sender
+{
+    self.colorAlert = [[UIAlertView alloc] initWithTitle:@"Choose a default color!"
+                                                    message:nil
+                                                   delegate:self
+                                          cancelButtonTitle:nil
+                                          otherButtonTitles:@"Purple", @"Blue", @"Orange", @"Green", nil];
+    self.colorAlert.tag = 1;
+    [self.colorAlert show];
 }
 
 @end
